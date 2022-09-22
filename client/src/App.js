5a280b76b6dea7eea8a5642bfe30b7eeda7b5aef
HastingsN3 <z1929228@students.niu.edu>
2022-09-22 14:26:43 -0500

    Updated Site

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Cookies } from 'react-cookie';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
@@ -8,8 +7,6 @@ import Login from './Pages/Login/Login';
 import Jobs from './Pages/Jobs/Jobs';
 
 export default function App() {
-
-  const cookies = new Cookies();
 
   if(!localStorage.getItem("token"))
     return <Login />
