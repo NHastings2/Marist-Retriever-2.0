e52372374c95572433256e15f01e19b564265aa5
HastingsN3 <z1929228@students.niu.edu>
2022-09-27 17:03:45 -0500

    Added update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##Stop Service
+sudo systemctl stop marist
+
+#Update REPO
+git stash
+git pull
+
+## Make the JS file executable
+cd ./server
+sudo chmox +x index.js
+cd ..
+
+##Build new Website
+npm run Build
+
+##Restart Service
+sudo systemctl start marist
