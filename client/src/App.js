67ea806e0ab04144a9dbab54991a92568558f97f
HastingsN3 <z1929228@students.niu.edu>
2022-10-11 15:52:26 -0500

    Removed Console Log

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ class App extends React.Component
     //Get cookies from client
     const {cookies} = this.props;
 
-    console.log(!cookies.get('connect.sid'));
     //Check if user is logged in
     if(!cookies.get('connect.sid'))
       return <Login />
